a10b8f3f2bba217def05da64b72a40223bc56b00
Arianxx <34032486+Arianxx@users.noreply.github.com>
2018-10-04 12:55:10 +0800

    feat(footer): add the site description

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,7 +9,10 @@ export default class Footer extends PureComponent {
       <footer>
         <div className='container text-center footer_text'>
           <p>
-            Copyright (c) 2018 Arianx
+            This site benefits from <a href='https://www.v2ex.com/p/7v9TEc53' target="_blank">V2EX API</a>.
+          </p>
+          <p>
+            Copyright (c) 2018 <a href="https://arianx.me" target="_blank">ArianX</a>
           </p>
         </div>
       </footer>
